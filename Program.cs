676229f59d17f57c0101a0ed675005c7f195532e
derrick <derrick.serbia@gmail.com>
2024-07-11 20:47:48 -0400

    swagger added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "JobTracker API",
+        Description = "Track you job applications",
+        Version = "v1"});
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobTracker API V1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
